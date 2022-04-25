1e90efbed05c4ec32f8cd62470c86dd1409ca2de
Hernan Oliva <hhernanoliva@gmail.com>
2022-04-25 01:25:13 -0300

    primeras impresiones

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css";
+  </script>
+  
+  <nav class="flex w-full justify-center text-neutral-800 bg-red-200">
+      <h1 class="text-4xl py-5">Agenda JS</h1>
+  </nav>
+  <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import agenda from './contactos.json';
+
+    let contactos = agenda;
+
+    $: contactos
+
+    let contactName
+    let contactEmail 
+
+
+    const addContact = (name, email) => {
+        const contact = {
+            name,
+            email
+        }
+        contactos.push(contact)
+    };
+    
+</script>
+
+
+<div class="container flex justify-center items-center mt-10 flex-col w-full">
+    <div class="text-xl">
+        {#each contactos as {name, email}}
+             <h2>{name}</h2>
+             <p>{email}</p>   
+        {/each}
+    </div>
+
+    <input placeholder="Name" bind:value={contactName}>
+    <input placeholder="Email" bind:value={contactEmail}>
+    <button on:click={addContact({contactName}, {contactEmail}, console.log(contactos))}> Click</button>
+    
+</div>
